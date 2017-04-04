7fa3551afe9745ed6508804bb535ab9fac16e189
Srajan Ratti <msfreedom911@gmail.com>
2017-04-04 13:39:49 +0530

    Initial Commit

---
+++ Source/FinalRockstar/FinalRockstar.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "FinalRockstar.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FinalRockstar, "FinalRockstar" );
+ 
---
+++ Source/FinalRockstar/FinalRockstar.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __FINALROCKSTAR_H__
+#define __FINALROCKSTAR_H__
+
+#include "EngineMinimal.h"
+
+#endif
